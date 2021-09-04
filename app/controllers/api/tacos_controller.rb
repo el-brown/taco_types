a97fae188664d16c2e6c849556179cd93bbac73c
Elise Brown <elisemarie88@gmail.com>
2021-09-04 11:45:32 -0600

    controller crud

--- app/controllers/api/tacos_controller.rb
+++ app/controllers/api/tacos_controller.rb
@@ -4,5 +4,32 @@ class Api::TacosController < ApplicationController
     render json: Taco.all
   end
 
-  
+  def create
+    taco = Taco.new(taco_params)
+    taco.likes = 0
+    if (taco.save)
+      render json: taco
+    else
+      render json: {erros: item.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    taco = Taco.find(params[:id])
+    taco.update(complete: !taco.complete)
+    render json: taco
+  end
+
+  def destroy
+    Taco.find(params[:id]).destroy
+    render json: { message: "Taco deleted" }
+  end
+
+
+  private
+
+  def taco_params
+    params.require(:taco).permit(:name, :ingredients)
+  end
+
 end
